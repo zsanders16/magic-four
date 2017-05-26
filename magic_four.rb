0a77f8f4219a7df74ca5a2c756afa9c4428f9f51
Zachary Sanders <zsanders@icloud.com>
2017-05-26 09:58:21 -0600

    add validation

--- magic_four.rb
+++ magic_four.rb
@@ -9,10 +9,16 @@ end
 def get_user_response
     puts "Please input a positive number."
 
-    user_number = gets.chomp.to_i.abs
+    user_number = gets.chomp
     
-    display_results(user_number)
-
+    
+    if validate?(user_number)
+        display_results(user_number.to_i)
+    else
+        puts "Invalid response, try again"
+        get_user_response
+    end
+    
 end
 
 
@@ -26,6 +32,10 @@ def display_results(number)
         display_results(number_length)
     end
     
+end
+
+def validate?(number)
+    !!(number =~ /^\d+$/ )
 end
 
 
